6c649279485edb71eead81f380b09cadd8dfba08
Shay <43632522+huyhandes@users.noreply.github.com>
2024-09-21 01:35:01 +0700

    feat(tmux) intergate with sesh

--- shell/tmux-custom/mode_indicator.sh
+++ shell/tmux-custom/mode_indicator.sh
@@ -3,8 +3,8 @@ show_mode_indicator() { # This function name must match the module name!
 
   index=$1 # This variable is used internally by the module loader in order to know the position of this module
 
-  icon="$(  get_tmux_option "@catppuccin_mode_indicator_icon"  ""           )"
-  color="$( get_tmux_option "@catppuccin_mode_indicator_color" "$thm_bg" )"
+  icon="$(  get_tmux_option "@catppuccin_mode_indicator_icon"  "" )"
+  color="$( get_tmux_option "@catppuccin_mode_indicator_color" "$thm_red" )"
   text="$(  get_tmux_option "@catppuccin_mode_indicator_text"  "#{tmux_mode_indicator}" )"
 
   module=$( build_status_module "$index" "$icon" "$color" "$text" )
