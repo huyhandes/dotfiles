ca33fb2ce695a418a61b8e1dac62538077c7ee1c
Shay <43632522+huyhandes@users.noreply.github.com>
2024-10-10 10:05:10 +0700

    refactor(starship): move into folder

--- config/nvim/lua/plugins/nvim-surround.lua
+++ config/nvim/lua/plugins/nvim-surround.lua
@@ -5,6 +5,33 @@ return {
   config = function()
     require("nvim-surround").setup({
       -- Configuration here, or leave empty to use defaults
+      keymaps = {
+        insert = "<C-s>",
+        normal = "s",
+        normal_cur = "ss",
+        visual = "s",
+      },
+      aliases = {
+        ["a"] = ">",
+        ["p"] = ")",
+        ["c"] = "}",
+        ["r"] = "]",
+        ["b"] = { "}", "]", ")", ">" },
+
+        ["A"] = "<",
+        ["P"] = "(",
+        ["C"] = "{",
+        ["R"] = "[",
+        ["B"] = { "<", "(", "{", "[" },
+
+        ["j"] = '"',
+        ["k"] = "'",
+        ["l"] = "`",
+        ["q"] = { '"', "'", "`" },
+
+        ["s"] = { "}", "]", ")", ">", '"', "'", "`" },
+      },
+      indent_lines = false,
     })
   end,
 }
