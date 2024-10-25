c7c2164eabc51d97318bd42d4d983254fb0c41f2
Shay <43632522+huyhandes@users.noreply.github.com>
2024-10-25 21:53:43 +0700

    update nvim config

---
+++ config/nvim/lua/plugins/lsp/nvim-cmp.lua
@@ -0,0 +1,30 @@
+return {
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  opts = function(_, opts)
+    opts.sources = opts.sources or {}
+    table.insert(opts.sources, {
+      name = "lazydev",
+      group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+    })
+  end,
+  config = function()
+    local cmp = require("cmp")
+
+    cmp.setup({
+      sources = {
+        { name = "nvim_lsp" },
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-d>"] = cmp.mapping.scroll_docs(4),
+      }),
+      snippet = {
+        expand = function(args)
+          vim.snippet.expand(args.body)
+        end,
+      },
+    })
+  end,
+}
