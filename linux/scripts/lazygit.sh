87ab6bf0107d05cdf7d0bb7f918b184ed4afeba8
Shay <43632522+huyhandes@users.noreply.github.com>
2024-10-28 16:38:05 +0700

    fix(lazygit): wrong variable script

--- linux/scripts/lazygit.sh
+++ linux/scripts/lazygit.sh
@@ -3,7 +3,7 @@
 ## Install lazygit - require root
 if test ! -f $LOCAL_BIN/lazygit; then
   LAZYGIT_VERSION="0.44.0"
-  wget "https://github.com/jesseduffield/lazygit/releases/download/v$LAZYGIT_VERSION/lazygit_$LAZYGIT_VERSION_Linux_x86_64.tar.gz" -O tmp
+  wget "https://github.com/jesseduffield/lazygit/releases/download/v$LAZYGIT_VERSION/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz" -O tmp
   mkdir lazygit 
   tar xf tmp -C lazygit
   
