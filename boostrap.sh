9c663c5cd727daa2ab32884842e46b447e99f18c
Shay <43632522+huyhandes@users.noreply.github.com>
2024-11-08 00:15:56 +0700

    refactor(zsh): remove omz, switch to zim

--- boostrap.sh
+++ boostrap.sh
@@ -14,15 +14,7 @@ else
   echo "This script only works on macOS and linux!"
 fi
 
-# Check for Oh My Zsh and install if we don't have it
-if test ! $(which omz); then
-  export ZSH="$HOME/.oh-my-zsh"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended --skip-chsh --keep-zshrc"
-fi
-
 # Add tmux plugin manager
 if test ! -d $HOME/.tmux/plugins/tpm; then
   git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 fi
-
-
