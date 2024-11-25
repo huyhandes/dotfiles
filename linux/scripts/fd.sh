cd1180af7e95ab653c805ebb90134920397f85e4
Shay <43632522+huyhandes@users.noreply.github.com>
2024-11-25 15:18:35 +0700

    fix(tmux): use fzf builtin, fix(fd): wrong folder

--- linux/scripts/fd.sh
+++ linux/scripts/fd.sh
@@ -6,7 +6,7 @@ if test ! -f $LOCAL_BIN/fd || [ $(fd --version | cut -d ' ' -f 2) != $FD_VERSION
   wget "https://github.com/sharkdp/fd/releases/download/v$FD_VERSION/fd-v$FD_VERSION-x86_64-unknown-linux-musl.tar.gz" -O tmp
   tar xzf tmp
   rm tmp
-  dir="fd-$FD_VERSION-x86_64-unknown-linux-musl"
+  dir="fd-v$FD_VERSION-x86_64-unknown-linux-musl"
 
   mkdir -p $LOCAL_BIN
   cp $dir/fd $LOCAL_BIN/
