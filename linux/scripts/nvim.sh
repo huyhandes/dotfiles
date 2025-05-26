8a570170cdf0c9bd4b76fabab8a4199da4bf7884
Shay <43632522+huyhandes@users.noreply.github.com>
2025-05-26 09:44:31 +0700

    fix nvim script

--- linux/scripts/nvim.sh
+++ linux/scripts/nvim.sh
@@ -6,7 +6,7 @@ if test ! -d $LOCAL_BIN/nvim-linux64 || [ $(nvim -v | grep "NVIM" | cut -d ' ' -
   wget "https://github.com/neovim/neovim/releases/download/$NVIM_VERSION/nvim-linux-x86_64.tar.gz"
   tar xzf nvim-linux-x86_64.tar.gz
   mkdir -p $LOCAL_BIN
-  mv  nvim-linux-x86_64$LOCAL_BIN/
+  mv  nvim-linux-x86_64 $LOCAL_BIN/
   rm -f $LOCAL_BIN/nvim
   ln -s $LOCAL_BIN/nvim-linux-x86_64/bin/nvim $LOCAL_BIN/nvim
   rm -r nvim-linux-x86_64*
