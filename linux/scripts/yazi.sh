ead21b43633c97a8babcb4f7cb3fe716bbbe53e2
Shay <43632522+huyhandes@users.noreply.github.com>
2025-10-03 08:53:44 +0700

    chore: switch yazi to $HOME

--- linux/scripts/yazi.sh
+++ linux/scripts/yazi.sh
@@ -4,19 +4,19 @@
 
 YAZI_VERSION="25.5.31"
 
-if test ! -f $LOCAL_BIN/yazi || [ $(yazi --version | cut -d ' ' -f 2) != $YAZI_VERSION ]; then
+if test ! -f $HOME/.local/bin/yazi || [ $(yazi --version | cut -d ' ' -f 2) != $YAZI_VERSION ]; then
   wget "https://github.com/sxyazi/yazi/releases/download/v$YAZI_VERSION/yazi-x86_64-unknown-linux-musl.zip" -O tmp
   unzip tmp
   rm tmp
   dir="yazi-x86_64-unknown-linux-musl"
 
-  mkdir -p $LOCAL_BIN
-  cp $dir/ya $LOCAL_BIN/
-  cp $dir/yazi $LOCAL_BIN/
+  mkdir -p $HOME/.local/bin
+  cp $dir/ya $HOME/.local/bin/
+  cp $dir/yazi $HOME/.local/bin/
 
-  mkdir -p $LOCAL_SHARE/zsh/completions
-  cp $dir/completions/_ya $LOCAL_SHARE/zsh/completions/_ya
-  cp $dir/completions/_yazi $LOCAL_SHARE/zsh/completions/_yazi
+  mkdir -p $$HOME/.local/share/zsh/completions
+  cp $dir/completions/_ya $$HOME/.local/share/zsh/completions/_ya
+  cp $dir/completions/_yazi $$HOME/.local/share/zsh/completions/_yazi
   
   rm -r $dir
 fi
